915206394dc35fe144a02a6050a87b7f1c6b5414
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2017-10-29 15:53:03 +0530

    delete sea.py and sms.py

--- sea/sms.py
+++
@@ -1,8 +0,0 @@
-import nexmo
-def function():
-    client = nexmo.Client(key='3a9542ef', secret='e29280bddcd6e45f')
-    client.send_message({
-        'from': 'TheGitStars',
-        'to': '918919067237',
-        'text': 'Lanja'
-    })
