594be5c4456a86ef29b0f66711e1016079fc1895
Saicharan Reddy <saicharan.reddy1@gmail.com>
2017-10-29 12:12:24 +0530

    Added functionalities

--- sea/sea.py
+++ sea/sea.py
@@ -1,13 +1,21 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def main():
-    return render_template('places.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@app.route('/places')
+def places():
+        return render_template('places.html')
+
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
